d5af856d60e6875fef8f5c216d5ffcf468505aa1
spicysos <cchae216@gmail.com>
2020-11-21 13:56:45 -0500

    finish basic design with css grid and material ui

--- src/BookList.js
+++ src/BookList.js
@@ -1,10 +1,26 @@
 import React,{Component} from 'react';
+import Box from '@material-ui/core/Box';
+import { Card,  CardHeader } from '@material-ui/core';
 
 class BookList extends Component {
     render() {
+        // process info here
+        
+        console.log(this.props.data)
         return(
-            <div>
-                Book List
+            <div className="list-container">
+                <Box>
+                <div className="wrapper">
+                    {this.props.data.map((i, idx) => {
+                        return (
+                            <Card key={idx} className="card-container" >
+                                <CardHeader className="title-box" title={i.volumeInfo.title} />
+                                <img alt="book thumbnail" className="img-box" src={i.volumeInfo.imageLinks.thumbnail}/>
+                            </Card>
+                        )
+                    })}
+                </div>
+                </Box>
             </div>
         )
     }
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,5 +1,7 @@
 
 import React,{ Component } from 'react';
+import { Button } from '@material-ui/core';
+import { Input } from '@material-ui/core';
 
 export default class SearchBox extends Component {
     constructor(props){
@@ -8,14 +10,28 @@ export default class SearchBox extends Component {
             haha: 1
         }
     }
+    componentDidMount(){
+        document.getElementById("search-input-box").addEventListener("keyup", function(event) {
+            event.preventDefault();
+            if (event.keyCode === 13) {
+                document.getElementById("search-button").click();
+            }
+        });
+    }
     render() {
         return (
-            <input 
-                type="search" 
-                className="search" 
-                placeholder={this.props.placeholder}
-                onChange={this.props.handleChange}
-            />
+            <div>
+
+                <Input 
+                    id="search-input-box"
+                    type="search" 
+                    size='medium'
+                    className="search" 
+                    placeholder={this.props.placeholder}
+                    onChange={this.props.handleChange}
+                />
+                <Button id="search-button" onClick={()=>this.props.handleSearchClick()} variant="contained" color="secondary">Search</Button>
+            </div>
         )
     }
     

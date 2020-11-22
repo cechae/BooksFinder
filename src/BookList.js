47474e43fa1b62696fe8d82082e4e8737c24a3d1
spicysos <cchae216@gmail.com>
2020-11-21 22:31:27 -0500

    add error handling

--- src/BookList.js
+++ src/BookList.js
@@ -1,22 +1,23 @@
 import React,{Component} from 'react';
 import Box from '@material-ui/core/Box';
 import { Card } from '@material-ui/core';
-
+import NonFound from './NonFound';
 class BookList extends Component {
     render() {
         // process info here
-        
-        console.log(this.props.data)
+        let isResultUndefined = this.props.data==undefined;
+       
         return(
             <div className="list-container">
                 <Box>
-                <div className="wrapper">
+                { !isResultUndefined ?
+                  <div className="wrapper">
                     {this.props.data.map((i, idx) => {
                         return (
                             <Card key={idx} className="card-container" >
                                 <div className="title-box">
                                     <h2>{i.volumeInfo.title}</h2>
-                                    <div className="small-desc">By: {i.volumeInfo.authors[0]}</div>
+                                    <div className="small-desc">By: {i.volumeInfo.authors!=undefined?i.volumeInfo.authors[0]:"Unknown"}</div>
                                     <div className="small-desc">Published by: {i.volumeInfo.publisher}</div>
                                 </div>
                                 <img alt="book thumbnail" className="img-box" src={i.volumeInfo.imageLinks.thumbnail}/>
@@ -24,6 +25,8 @@ class BookList extends Component {
                         )
                     })}
                 </div>
+                :
+                <NonFound />}
                 </Box>
             </div>
         )
---
+++ src/NonFound.js
@@ -0,0 +1,8 @@
+
+
+import React from 'react';
+
+function NonFound() {
+    return <h1> Oops... No books found. Try other search. </h1>;
+}
+export default NonFound;

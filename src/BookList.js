e4c3d19f00bc0e06ce6a3da64f35aca13bba090f
spicysos <cchae216@gmail.com>
2020-11-22 14:55:15 -0500

    add error handling

--- src/BookList.js
+++ src/BookList.js
@@ -1,11 +1,12 @@
 import React,{Component} from 'react';
 import Box from '@material-ui/core/Box';
 import { Card } from '@material-ui/core';
-import NonFound from './NonFound';
+
+const defaultBookImg = "";
 class BookList extends Component {
     render() {
         // process info here
-        let isResultUndefined = this.props.data==undefined;
+        let isResultUndefined = this.props.data===undefined;
        
         return(
             <div className="list-container">
@@ -17,16 +18,19 @@ class BookList extends Component {
                             <Card key={idx} className="card-container" >
                                 <div className="title-box">
                                     <h2>{i.volumeInfo.title}</h2>
-                                    <div className="small-desc">By: {i.volumeInfo.authors!=undefined?i.volumeInfo.authors[0]:"Unknown"}</div>
-                                    <div className="small-desc">Published by: {i.volumeInfo.publisher}</div>
+                                    <div className="small-desc">By: {i.volumeInfo.authors !== undefined ? i.volumeInfo.authors[0] : "Unknown"}</div>
+                                    <div className="small-desc">Published by: {i.volumeInfo.publisher !== undefined ? i.volumeInfo.publisher : "Unknown"}</div>
+                                    <div></div>
                                 </div>
-                                <img alt="book thumbnail" className="img-box" src={i.volumeInfo.imageLinks.thumbnail}/>
+                                <img alt="book thumbnail" className="img-box" src={i.volumeInfo.imageLinks !== undefined ? i.volumeInfo.imageLinks.thumbnail : defaultBookImg}/>
                             </Card>
                         )
                     })}
                 </div>
                 :
-                <NonFound />}
+                <div className="non-found">
+                    <h1> Oops... No books found. Try other search. </h1>
+                </div>}
                 </Box>
             </div>
         )
--- src/NonFound.js
+++
@@ -1,8 +0,0 @@
-
-
-import React from 'react';
-
-function NonFound() {
-    return <h1> Oops... No books found. Try other search. </h1>;
-}
-export default NonFound;

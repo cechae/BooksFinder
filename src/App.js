efb9f195b8debb0c9375935a46ea8727610351c8
spicysos <cchae216@gmail.com>
2020-11-26 16:04:04 -0500

    fix desktop design

--- src/App.js
+++ src/App.js
@@ -11,50 +11,49 @@ class App extends React.Component {
     super();
     this.state = {
       books: [],
-      searchField: "placeholder"
+      searchField: "placeholder",
+      isAppMounted: false,
     }
   }
 
   handleSearchClick = () => {
+    if (!this.state.searchField.trim()) {
+      // empty string, add a warning that says "Please input search words here. "
+    }
+    this.setState({isAppMounted: false})
     //fetch request will be sent when the search button is clicked
     console.log("search field was " + this.state.searchField)
     fetch(`https://www.googleapis.com/books/v1/volumes?q=${this.state.searchField}&key=${apiKey}`)
       .then(response => response.json())
       .then(result => {
-        this.setState({books: result.items})
+        this.setState({books: result.items, isAppMounted: true})
       });
   }
   
   componentDidMount() {
-    
     // Initial fetch request to fill up the placeholder book list.
     if (this.state.searchField === "placeholder") {
       // run a default books search
-      fetch(`https://www.googleapis.com/books/v1/volumes?q=search-terms&key=${apiKey}`)
+      fetch(`https://www.googleapis.com/books/v1/volumes?q=books&key=${apiKey}`)
       .then(response => response.json())
       .then(result => {
-        this.setState({books: result.items})
+        this.setState({books: result.items, isAppMounted: true })
       });
     } 
-    
-    
   }
   render() {
-    
     return (
       <div className="App">
         <div className="container">
           <Box>
             <Title/>
             <SearchBox handleSearchClick={this.handleSearchClick} placeholder="Search books..." handleChange={(e) => this.setState({searchField: e.target.value})} />
-            <BookList data={this.state.books} />
+            <BookList data={this.state.books} isMounted={this.state.isAppMounted}/>
           </Box>
         </div>
       </div>
     );
-
   }
-  
 }
 
 export default App;
--- src/BookList.js
+++ src/BookList.js
@@ -1,39 +1,42 @@
-import React,{Component} from 'react';
+import React from 'react';
 import Box from '@material-ui/core/Box';
 import { Card } from '@material-ui/core';
+import { Button } from '@material-ui/core';
 
-const defaultBookImg = "";
-class BookList extends Component {
-    render() {
-        // process info here
-        let isResultUndefined = this.props.data===undefined;
-       
-        return(
-            <div className="list-container">
-                <Box>
-                { !isResultUndefined ?
-                  <div className="wrapper">
-                    {this.props.data.map((i, idx) => {
-                        return (
-                            <Card key={idx} className="card-container" >
-                                <div className="title-box">
-                                    <h2>{i.volumeInfo.title}</h2>
-                                    <div className="small-desc">By: {i.volumeInfo.authors !== undefined ? i.volumeInfo.authors[0] : "Unknown"}</div>
-                                    <div className="small-desc">Published by: {i.volumeInfo.publisher !== undefined ? i.volumeInfo.publisher : "Unknown"}</div>
-                                    <div></div>
-                                </div>
-                                <img alt="book thumbnail" className="img-box" src={i.volumeInfo.imageLinks !== undefined ? i.volumeInfo.imageLinks.thumbnail : defaultBookImg}/>
-                            </Card>
-                        )
-                    })}
-                </div>
+const BookList = (props) => {
+    let isResultUndefined = !props.data;
+    return(
+        <div className="list-container">
+            {!props.isMounted ?
+                <p className="loading-p">Loading<span></span></p>
                 :
-                <div className="non-found">
-                    <h1> Oops... No books found. Try other search. </h1>
-                </div>}
+                <Box>
+                    { !isResultUndefined ?
+                    <div className="wrapper">
+                        {props.data.map((i, idx) => {
+                            return (
+                                <Card key={idx} className="card-container" >
+                                    <div className="title-box">
+                                        <h2>{i.volumeInfo.title}</h2>
+                                        <div className="small-desc">By: {i.volumeInfo.authors !== undefined ? i.volumeInfo.authors[0] : "Unknown"}</div>
+                                        <div className="small-desc">Published by: {i.volumeInfo.publisher !== undefined ? i.volumeInfo.publisher : "Unknown"}</div>
+                                        <div  className="small-desc button-div">
+                                            <Button id="search-button" variant="contained" color="secondary">See this Book</Button>
+                                        </div>
+                                        
+                                    </div>
+                                    <img alt="book thumbnail" className="img-box" src={i.volumeInfo.imageLinks !== undefined ? i.volumeInfo.imageLinks.thumbnail : ""}/>
+                                </Card>
+                            )
+                        })}
+                    </div>
+                    :
+                    <div className="non-found">
+                        <h1> Oops... No books found. Try other search. </h1>
+                    </div>}
                 </Box>
-            </div>
-        )
-    }
+                }
+        </div>
+    )
 }
 export default BookList;
